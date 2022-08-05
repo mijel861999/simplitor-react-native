d78a14cc036c02740bcb82cdc8bb4e39ce23e687
mijelpalcabello <miguel.castillo861999@gmail.com>
2022-08-05 16:53:25 -0500

    Agregando botón para regresar

--- screens/note-screen.js
+++ screens/note-screen.js
@@ -1,22 +1,26 @@
 import React from 'react'
-import { Box, Text, VStack, TextArea } from 'native-base'
-import { Keyboard, TouchableWithoutFeedback, View, ScrollView } from 'react-native'
-import DismissKeyboardHOC from '../components/keyboard'
+import { ArrowBack, Box, Text, VStack, TextArea, Button } from 'native-base'
+import { Keyboard, TouchableWithoutFeedback, View } from 'react-native'
 
 const NoteScreen = () => {
   return (
     <VStack flex={1} _dark={{ bg: 'blueGray.900' }} _light={{ bg: 'blueGray.50' }} flex={1}>
       <Box p={4}>
+        <Button w={120}>Atrás</Button>
         <Text>Simplitor</Text>
       </Box>
-      <DismissKeyboardHOC>
-        <TextArea
-          h={100} w='full'
-          borderColor='blueGray.900'
-          placeholder="Text Area Placeholder"
-          _dark={{ color: 'white' }} _light={{ bg: 'black' }}
-        />
-      </DismissKeyboardHOC>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+        <View style={{flex: 1}}>
+          <TextArea
+            _dark={{ color: 'blueGray.50', bg: 'blueGray.900' }}
+            _light={{ color: 'blueGray.900', bg: 'blueGray.50' }}
+            h={100}
+            borderWidth={0}
+            placeholder="Text Area Placeholder"
+            w="100%"
+          />
+        </View>
+      </TouchableWithoutFeedback>
     </VStack>
   )
 }
