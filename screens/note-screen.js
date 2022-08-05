dab8cacb32431366270c233d7b01a165f8f60561
mijelpalcabello <miguel.castillo861999@gmail.com>
2022-08-05 15:35:13 -0500

    Add a keyboard that can be hiden

--- screens/note-screen.js
+++ screens/note-screen.js
@@ -1,15 +1,22 @@
 import React from 'react'
 import { Box, Text, VStack, TextArea } from 'native-base'
+import { Keyboard, TouchableWithoutFeedback, View, ScrollView } from 'react-native'
+import DismissKeyboardHOC from '../components/keyboard'
 
 const NoteScreen = () => {
   return (
-    <VStack flex={1}>
-      <Box>
+    <VStack flex={1} _dark={{ bg: 'blueGray.900' }} _light={{ bg: 'blueGray.50' }} flex={1}>
+      <Box p={4}>
         <Text>Simplitor</Text>
       </Box>
-      <Box>
-        <TextArea h='full' placeholder="Text Area Placeholder" /> 
-      </Box>
+      <DismissKeyboardHOC>
+        <TextArea
+          h={100} w='full'
+          borderColor='blueGray.900'
+          placeholder="Text Area Placeholder"
+          _dark={{ color: 'white' }} _light={{ bg: 'black' }}
+        />
+      </DismissKeyboardHOC>
     </VStack>
   )
 }
